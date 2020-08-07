decdb28342137e970e69c750a334e964e850f3a3
dpoulson <darren.poulson@gmail.com>
2020-08-07 01:05:32 +0100

    Added members table definition

--- database/migrations/2020_08_01_234151_create_members_table.php
+++ database/migrations/2020_08_01_234151_create_members_table.php
@@ -15,6 +15,25 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->text('forename');
+            $table->text('surname');
+            $table->char('county', 50);
+            $table->char('postcode', 50);
+            $table->char('latitude', 16);
+            $table->char('longitude', 16);
+            $table->text('email');
+            $table->char('username', 25);
+            $table->date('pli_date');
+            $table->boolean('pli_active');
+            $table->boolean('active');
+            $table->integer('created_by');
+            $table->string('password');
+            $table->boolean('force_password');
+            $table->string('gdpr_accepted');
+            $table->timestamp('last_login',0);
+            $table->char('last_login_from', 16);
+            $table->string('badge_id');
+            $table->text('remember_token');
             $table->timestamps();
         });
     }
