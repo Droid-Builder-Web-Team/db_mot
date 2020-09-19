da68a0b1a0d661c0681b2437074d65be6baf5bdf
dpoulson <darren.poulson@gmail.com>
2020-09-19 02:31:30 +0100

    Forgot to save a file!

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -149,7 +149,9 @@
     </div>
   </div>
 </div>
-<div class="row">
+<div class="row">@php
+  $parsed_date = \Carbon\Carbon::createFromFormat('Y-m-d', $event->date);
+@endphp
   @can('Edit Members')
     <a class="btn btn-primary" href="{{ route('admin.users.edit',$user->id) }}">Edit</a>
   @else
