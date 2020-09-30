e524c1c2ecb07e47c739eed02bcaebb8791079b6
Darren Poulson <darren.poulson@gmail.com>
2020-10-01 00:39:45 +0100

    Update create.blade.php

--- resources/views/admin/droids/create.blade.php
+++ resources/views/admin/droids/create.blade.php
@@ -100,7 +100,7 @@
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4">
             <div class="form-group">
-                <strong>Approx Weight</strong> (in KG)
+                <strong>Approx Weight</strong> (in kg)
                 <input type="text" name="weight" class="form-control" placeholder="">
             </div>
         </div>
