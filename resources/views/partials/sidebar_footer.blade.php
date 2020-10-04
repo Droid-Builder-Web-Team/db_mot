5fe89d9f9d0f52d566ced327d4fc9c95b4adf5c7
Rob Howdle <robhowdle94@gmail.com>
2020-10-04 21:54:48 +0100

    Notifications Page

---
+++ resources/views/partials/sidebar_footer.blade.php
@@ -0,0 +1,12 @@
+<p class="sideFooter copyright-text">{{ config('app.name', 'Laravel') }} <?php echo date("Y");?> Created and Maintained by Droid Builder Web Team
+    <br />
+  <a href="https://coreui.io" style="color:white;">CoreUI</a> © 2020 creativeLabs.
+  Powered by&nbsp;<a href="https://coreui.io/pro/" style="color:white;">CoreUI Pro</a>
+</p>
+<ul class="social-icons list-group d-flex">
+    <li class="list-group-item"><a class="facebook" href="https://www.facebook.com/groups/droidbuildersuk"><i class="fab fa-facebook"></i></a></li>
+    <li class="list-group-item"><a class="twitter" href="https://twitter.com/droidbuildersuk"><i class="fab fa-twitter"></i></a></li>
+    <li class="list-group-item"><a class="dribbble" href="https://discord.gg/5D4QE4"><i class="fab fa-discord"></i></a></li>
+    <li class="list-group-item"><a class="youtube" href="https://www.youtube.com/c/DroidbuildersUK"><i class="fab fa-youtube"></i></a></li>
+    <li class="list-group-item"><a class="instagram" href="https://www.instagram.com/droidbuildersuk/"><i class="fab fa-instagram"></i></a></li>
+</ul>
