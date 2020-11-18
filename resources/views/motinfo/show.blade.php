16322233f1137bfddd6f24f03d1eaa7e88d83cf1
dpoulson <darren.poulson@gmail.com>
2020-11-18 23:53:52 +0000

    Updates to the motinfo page.

--- resources/views/motinfo/show.blade.php
+++ resources/views/motinfo/show.blade.php
@@ -9,8 +9,8 @@
       <div class="card-header">
         <h4 class="title text-center">MOT Info</h4>
       </div>
-      <div class="card-body ">
-          <div class="mb-4">
+      <div class="card-body mb-2">
+          <div>
             <p>Why MOT? Droids can be rather heavy and dangerous if built wrong. The MOT process is a way to make sure your droid is safe for taking out in public and
               is unlikely to cause any injuries or issues. Not just to the people you interact with, but also the environment itself. A 60kg+ runaway droid can do a lot of
               damage to stalls, cars, etc. After your droid has passed its MOT, you can then join in the club PLI to make sure you are insured incase of any incidents. The MOT process is
@@ -36,22 +36,22 @@
             <div class="accordion" id="accordionMOT">
           @foreach($sections as $section)
 
-              <div class="card">
+              <div class="card-mot">
                 <div class="card-header-mot" id="heading{{ $section->id}}">
-                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse{{ $section->id}}" aria-expanded="false" aria-controls="collapse{{ $section->id}}">
+                  <h5 class="mb-0 d-inline">
+                  <button class="btn btn-motinfo collapsed" type="button" data-toggle="collapse" data-target="#collapse{{ $section->id}}" aria-expanded="false" aria-controls="collapse{{ $section->id}}">
                     {{ $section->section_description }}
                   </button>
+                </h5>
                 </div>
                 <div id="collapse{{ $section->id}}" class="collapse" aria-labelledby="heading{{ $section->id}}" data-parent="#accordionMOT">
                   <div class="card-body">
-                    <div class="mb-4">{!! nl2br(e($section->section_long_description)) !!}</div>
-
-
+                    <div class="mb-2">{!! nl2br(e($section->section_long_description)) !!}</div>
                     <div class="accordion" id="accordionLines{{ $section->id}}">
                       @foreach($lines[$section->id] as $line)
-                        <div class="card">
+                        <div class="card-mot">
                           <div class="card-header-mot" id=headingLine{{ $line->id}}">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseLine{{ $line->id}}" aria-expanded="false" aria-controls="collapseLine{{ $line->id}}">
+                            <button class="btn btn-motinfo collapsed" type="button" data-toggle="collapse" data-target="#collapseLine{{ $line->id}}" aria-expanded="false" aria-controls="collapseLine{{ $line->id}}">
                               {{ $line->test_description }}
                             </button>
                           </div>
