cca89a6d0b70883fc3edf917bd2e4f49373d4633
dpoulson <darren.poulson@gmail.com>
2021-06-09 15:53:06 +0100

    Seems it is a php memory limit

--- config/backup.php
+++ config/backup.php
@@ -18,7 +18,7 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    base_path(),
+                    base_path('storage'),
                 ],
 
                 /*
@@ -27,8 +27,6 @@ return [
                  * Directories used by the backup process will automatically be excluded.
                  */
                 'exclude' => [
-                    base_path('vendor'),
-                    base_path('node_modules'),
                 ],
 
                 /*
