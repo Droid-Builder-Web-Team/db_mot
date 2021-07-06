bb3f26e66b08adda5f6871cc6378d70ebb88068f
patrickatkeylogic <39687831+patrickatkeylogic@users.noreply.github.com>
2021-07-06 08:42:30 -0400

    Bit of stylesheet organization

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-if (mix.inProduction()) {
-    mix.version();
-}
+mix.js('resources/js/app.js', 'public/js').version();
+mix.sass('resources/sass/app.scss', 'public/css').version();
