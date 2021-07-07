317b0f599d8655d61ff0ae9737c44e026a8e86cf
patrickatkeylogic <pat.m.ryan@gmail.com>
2021-07-06 20:39:33 -0400

    Removing light/dark toggle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
-        integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
     <link href="{{ asset('css/datatables.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
@@ -20,9 +19,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.21/datatables.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"
-        integrity="sha512-yUNtg0k40IvRQNR20bJ4oH6QeQ/mgs9Lsa6V+3qxTj58u2r+JiAYOhOW0o+ijuMmqCtCEg7LZRA+T4t84/ayVA=="
-        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js" integrity="sha512-yUNtg0k40IvRQNR20bJ4oH6QeQ/mgs9Lsa6V+3qxTj58u2r+JiAYOhOW0o+ijuMmqCtCEg7LZRA+T4t84/ayVA==" crossorigin="anonymous"></script>
 
     <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
 
@@ -38,44 +35,30 @@
 
     <div class="c-wrapper">
         <header class="c-header c-header-light c-header-fixed">
-            <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar"
-                data-class="c-sidebar-show">
+            <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show">
                 <i class="c-icon c-icon-lg cil-menu"></i>
             </button>
-            <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar"
-                data-class="c-sidebar-lg-show" responsive="true">
+            <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true">
                 <i class="c-icon c-icon-lg cil-menu"></i>
             </button>
-            {{-- <ul class="c-header-nav mfs-auto">
-                <li class="c-header-nav-item px-3 c-d-legacy-none">
-                    <button class="c-class-toggler c-header-nav-btn" type="button" id="header-tooltip"
-                        data-target="body" data-class="c-dark-theme" data-toggle="c-tooltip" data-placement="bottom"
-                        title="Toggle Light/Dark Mode">
-
-
-                        <i class="cil-moon c-d-dark-none"></i>
-                        <i class="cil-sun c-d-default-none"></i>
-                    </button>
-                </li>
-            </ul> --}}
             <ul class="c-header-nav mfs-auto">
-            @guest
-            <li class="c-header-nav-item mr-2">
-              <a class="btn btn-mot" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            @if (Route::has('register'))
-              <li class="c-header-nav-item mr-2">
-                <a class="btn btn-mot" href="{{ route('register') }}">{{ __('Register') }}</a>
-              </li>
-            @endif
-          @else
-          <li class="c-header-nav-item">
-            <a href="/Droid Builders Portal User Guide.pdf" target="_blank"><i class="far fa-question-circle fa-2x" title="User Guide"></i></a>
-          </li>
-            @include('partials.notification')
-            @include('partials.account')
+                @guest
+                    <li class="c-header-nav-item mr-2">
+                        <a class="btn btn-mot" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="c-header-nav-item mr-2">
+                            <a class="btn btn-mot" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="c-header-nav-item">
+                        <a href="/Droid Builders Portal User Guide.pdf" target="_blank"><i class="far fa-question-circle fa-2x" title="User Guide"></i></a>
+                    </li>
+                    @include('partials.notification')
+                    @include('partials.account')
 
-          @endguest
+                @endguest
             </ul>
         </header>
 
