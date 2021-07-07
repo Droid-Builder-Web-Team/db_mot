9c22b985f3ff13447a651b934566ccb8791a4cda
patrickatkeylogic <pat.m.ryan@gmail.com>
2021-07-06 20:51:53 -0400

    Open social links in new tab/window

--- resources/views/partials/sidebar_footer.blade.php
+++ resources/views/partials/sidebar_footer.blade.php
@@ -1,7 +1,7 @@
 <p class="sideFooter copyright-text">{{ config('app.name', 'Laravel') }} <?php echo date('Y'); ?> Created and Maintained by Droid Builder Web Team
 </p>
 <ul class="social-icons list-group flex-sm-row mx-auto">
-    <li class="list-group-item" id="facebookLi"><a class="facebook" href="https://www.facebook.com/groups/droidbuildersuk"><i class="fab fa-facebook"></i></a></li>
-    <li class="list-group-item" id="discordLi"><a class="dribbble" href="https://discord.gg/5D4QE4"><i class="fab fa-discord"></i></a></li>
-    <li class="list-group-item" id="youtubeLi"><a class="youtube" href="https://www.youtube.com/c/DroidbuildersUK"><i class="fab fa-youtube"></i></a></li>
+    <li class="list-group-item" id="facebookLi"><a class="facebook" target="_blank" href="https://www.facebook.com/groups/droidbuildersuk"><i class="fab fa-facebook"></i></a></li>
+    <li class="list-group-item" id="discordLi"><a class="dribbble" target="_blank" href="https://discord.gg/5D4QE4"><i class="fab fa-discord"></i></a></li>
+    <li class="list-group-item" id="youtubeLi"><a class="youtube" target="_blank" href="https://www.youtube.com/c/DroidbuildersUK"><i class="fab fa-youtube"></i></a></li>
 </ul>
