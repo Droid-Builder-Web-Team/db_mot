23e1b5a3caf5560b2958fb2d329fb8849aae748a
dpoulson <darren.poulson@gmail.com>
2021-07-08 09:31:19 +0100

    Added debugto paypal

--- resources/views/event/facebook.blade.php
+++ resources/views/event/facebook.blade.php
@@ -1,7 +1,7 @@
     <!-- Facebook metadata for sharing events -->
     <meta property="og:url"           content="{{ URL::current() }}" />
     <meta property="og:type"          content="website" />
-    <meta property="og:title"         content="{{ $event->name }} - {{ $event->location->name ($event->location->town) }} - {{ $event->date }}" />
+    <meta property="og:title"         content="{{ $event->name }} - {{ $event->location->name }} - {{ $event->date }}" />
     <meta property="og:description"   content="{{ $event->description }}" />
     <meta property="og:image"         content="https://{{ request()->getHttpHost() }}/images/new_event.jpg" />
     <meta property="fb:app_id"        content="{{ config('fb.fb_app_id', 'Laravel') }}" />
