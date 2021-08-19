081345b9ec87ca760d1d265eaa21254cdc5f80a1
Rob Howdle <robhowdle94@gmail.com>
2021-08-19 23:50:13 +0100

    Working on Updates/ Saves to Part Runs

--- app/Http/Controllers/PartsRunDataController.php
+++ app/Http/Controllers/PartsRunDataController.php
@@ -50,59 +50,57 @@ class PartsRunDataController extends Controller
      */
     public function store(Request $request)
     {
-            $user = Auth::user();
-
-            // Check image
-            if ($request->hasFile('image')) {
-                $request->validate([
-                    'image' => 'mimes:jpeg,bmp,png' // Only allow .jpg, .bmp and .png file types.
-                ]);
-            }
-
-            // Parts Run Data
-            $partsRunData = app(PartsRunData::class)->create([
-                'club_id' => $request->club_id,
-                'user_id' => $user->id,
-                'bc_rep_id' => $request->bc_rep_id,
-                'status' => 'Active',
+        // Check image
+        if ($request->hasFile('image')) {
+            $request->validate([
+                'image' => 'mimes:jpeg,bmp,png' // Only allow .jpg, .bmp and .png file types.
             ]);
+        }
 
-            $partsRunData->save();
+        // Parts Run Data
+        $partsRunData = app(PartsRunData::class)->create([
+            'club_id' => $request->club_id,
+            'user_id' => Auth::user()->id,
+            'bc_rep_id' => $request->bc_rep_id,
+            'status' => $request->status,
+        ]);
 
-            // Save image
-            $partsRunImage = $request->image->store('parts-run/'.$partsRunData->id.'/');
-            $partsRunImage = app(PartsRunImage::class)->create([
-                'parts_run_data_id' => $partsRunData->id,
-                'filename' => basename($partsRunImage),
-                'filetype' => $request->file('image')->getMimeType()
-            ]);
+        $partsRunData->save();
 
-            // Instructions Upload
-            $partsRunInstructions = $request->instructions->store('parts-run/'.$partsRunData->id.'/');
-            $partsRunInstructions = app(Instructions::class)->create([
-                'parts_run_data_id' => $partsRunData->id,
-                'filename' => basename($partsRunInstructions),
-                'filetype' => $request->file('instructions')->getMimeType()
+        // Save image
+        $partsRunImage = $request->image->store('parts-run/'.$partsRunData->id.'/');
+        $partsRunImage = app(PartsRunImage::class)->create([
+            'parts_run_data_id' => $partsRunData->id,
+            'filename' => basename($partsRunImage),
+            'filetype' => $request->file('image')->getMimeType()
+        ]);
 
-            ]);
+        // Instructions Upload
+        $partsRunInstructions = $request->instructions->store('parts-run/'.$partsRunData->id.'/');
+        $partsRunInstructions = app(Instructions::class)->create([
+            'parts_run_data_id' => $partsRunData->id,
+            'filename' => basename($partsRunInstructions),
+            'filetype' => $request->file('instructions')->getMimeType()
 
-            // Parts run data. To be created last
-            $partsRunAd = app(PartsRunAd::class)->create([
-                'parts_run_data_id' => $partsRunData->id,
-                'title' => $request->title,
-                'description' => $request->description,
-                'history' => $request->history,
-                'price' => $request->price,
-                'includes' => $request->includes,
-                'location' => $request->location,
-                'shipping_costs' => $request->shipping_costs,
-                'purchase_url' => $request->purchase_url,
-                'contact_email' => $request->contact_email,
-            ]);
+        ]);
 
-            $partsRunAd->save();
+        // Parts run data. To be created last
+        $partsRunAd = app(PartsRunAd::class)->create([
+            'parts_run_data_id' => $partsRunData->id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'history' => $request->history,
+            'price' => $request->price,
+            'includes' => $request->includes,
+            'location' => $request->location,
+            'shipping_costs' => $request->shipping_costs,
+            'purchase_url' => $request->purchase_url,
+            'contact_email' => $request->contact_email,
+        ]);
 
-            return redirect()->route('part-runs.index');
+        $partsRunAd->save();
+
+        return redirect()->route('part-runs.index');
     }
 
     /**
@@ -166,6 +164,7 @@ class PartsRunDataController extends Controller
     {
         $data = $request->all();
         $partsRunData = PartsRunData::findOrFail($id)->with('partsRunAd');
+        // dd($data);
         // $partsRunAd = PartsRunAd::findOrFail('parts_run_data_id', $id);
         $partsRunData->update($data);
     }
--- resources/views/part-runs/edit.blade.php
+++ resources/views/part-runs/edit.blade.php
@@ -12,144 +12,132 @@
                 <form action="{{ route('part-runs.update', $data->id) }}" method="POST">
                     @csrf
                     @method('PUT')
-                    <div class="form-row">
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Title</span>
-                                </div>
-                                <input type="text" name="title" value="{{ $data->partsRunAd->title }}" class="form-control" placeholder="Title">
+                     <div class="form-group row">
+                        <div class="col-12">
+                            <label for="parts_run_status" class="col-4 col-form-label">Parts Run Status</label> 
+                            <div class="col-12">
+                                <select id="parts_run_status" name="parts_run_status" value="{{ $data->status }}" class="custom-select" required="required">
+                                    <option value="Active">Active</option>
+                                    <option value="Inactive">Inactive</option>
+                                    <option value="Gathering_Interest">Gathering Interest</option>
+                                </select>
                             </div>
                         </div>
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Price</span>
-                                </div>
-                                <input type="number" name="price" value="{{ $data->partsRunAd->price }}" class="form-control" placeholder="Price">
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 col-12">
+                            <label for="title" class="col-4 col-form-label">Title</label> 
+                            <div class="col-12">
+                                <input type="text" name="title" value="{{ $data->partsRunAd->title }}" class="form-control" placeholder="Title" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-12">
+                            <label for="price" class="col-4 col-form-label">Price</label> 
+                            <div class="col-12">
+                            <input type="number" name="price" value="{{ $data->partsRunAd->price }}" class="form-control" placeholder="Price" required>
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                      <div class="mb-3 col-md-6">
-                          <div class="mb-3 input-group">
-                              <div class="input-group-prepend">
-                                  <span class="input-group-text">Club</span>
-                              </div>
-                              <select id="club_id" name="club_id">
-                                <option value="null">Select Club</option>
-                                @foreach($clubs as $club)
-                                  <option value={{ $club->id }}>{{ $club->name }}</option>
-                                @endforeach
-                              </select>
-                          </div>
-                      </div>
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">BC Rep</div>
-                                    <select id="bc_rep_id" name="bc_rep_id">
+                    <div class="form-group row">
+                        <div class="col-md-6 col-12">
+                            <label for="select" class="col-4 col-form-label">Club</label> 
+                            <div class="col-12">
+                                <select id="club_id" name="club_id" class="custom-select">
+                                    <option value="null">Select a club</option>
+                                    @foreach($clubs as $club)
+                                        <option value={{ $club->id }}>{{ $club->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-12">
+                            <label for="select1" class="col-4 col-form-label">BC Rep</label> 
+                            <div class="col-12">
+                                <select id="bc_rep_id" name="bc_rep_id" class="custom-select">
                                     <option value="null">Please select club first</option>
-                                    </select>
-                                </div>
+                                </select>
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Location</span>
-                                </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 col-12">
+                            <label for="location" class="col-4 col-form-label">Location</label> 
+                            <div class="col-12">
                                 <input type="text" name="location" value="{{ $data->partsRunAd->location }}" class="form-control">
                             </div>
                         </div>
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Shipping Costs</span>
-                                </div>
+
+                        <div class="col-md-6 col-12">
+                            <label for="shipping_costs" class="col-4 col-form-label">Shipping Costs</label> 
+                            <div class="col-12">
                                 <input type="text" name="shipping_costs" value="{{ $data->partsRunAd->shipping_costs }}" class="form-control">
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="mb-3 col-md-12">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Description</span>
-                                </div>
+                    <div class="form-group row">
+                        <div class="col-12">
+                            <label for="description" class="col-4 col-form-label">Description</label> 
+                            <div class="col-12">
                                 <input type="text" name="description" value="{{ $data->partsRunAd->description }}" class="form-control">
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Includes</span>
-                                </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 col-12">
+                            <label for="includes" class="col-4 col-form-label">Includes</label> 
+                            <div class="col-12">
                                 <textarea type="text" name="includes" value="{{ $data->partsRunAd->includes }}" class="form-control"></textarea>
                             </div>
                         </div>
-
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">History</span>
-                                </div>
+                        <div class="col-md-6 col-12">
+                            <label for="includes" class="col-4 col-form-label">History</label> 
+                            <div class="col-12">
                                 <textarea type="text" name="history" value="{{ $data->partsRunAd->history }}" class="form-control"></textarea>
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Purchase URL</span>
-                                </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 col-12">
+                            <label for="purchase_url" class="col-4 col-form-label">Purchase URL</label> 
+                            <div class="col-12">
                                 <input type="text" name="purchase_url" value="{{ $data->partsRunAd->purchase_url }}" class="form-control">
                             </div>
                         </div>
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Contact Email</span>
-                                </div>
+                        <div class="col-md-6 col-12">
+                            <label for="email" class="col-4 col-form-label">Email</label> 
+                            <div class="col-12">
                                 <input type="text" name="contact_email" value="{{ $data->partsRunAd->contact_email }}" class="form-control">
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Instructions</span>
-                                    <input type="file" name="instructions" class="form-control-file" id="exampleFormControlFile1">
-                                </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 col-12">
+                            <label for="text" class="col-4 col-form-label">Instructions</label> 
+                            <div class="col-12">
+                                <input type="file" name="instructions" class="form-control-file" id="exampleFormControlFile1">
                             </div>
                         </div>
-                        <div class="mb-3 col-md-6">
-                            <div class="mb-3 input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Image</span>
-                                    <input type="file" name="image" class="form-control-file" id="exampleFormControlFile1">
-                                </div>
+                        <div class="col-md-6 col-12">
+                            <label for="text1" class="col-4 col-form-label">Image</label> 
+                            <div class="col-12">
+                                <input type="file" name="image" class="form-control-file" id="exampleFormControlFile1">
                             </div>
-                            <img style="width: 400px; height: 400px;" src="{{ route('image.displayPartsRunImage', $data->id) }}" class="img-fluid">
+                            <div class="image-preview mt-3" style="width: 400px; height: 400px;">
+                                <img src="{{ route('image.displayPartsRunImage', $data->id) }}" class="img-fluid">
+                            </div>
                         </div>
-                    </div>
-
-
-                    <div class="form-row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 text-center mb-3">
-                        <button type="submit" style="width:auto;" class="btn btn-mot">Submit</button>
+                    </div> 
+                    <div class="form-group row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 text-center mt-3">
+                            <button type="submit" style="width:auto;" class="btn btn-mot">Submit</button>
                         </div>
                     </div>
                 </form>
