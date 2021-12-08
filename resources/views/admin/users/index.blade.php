198eaeb2b189942c443020c0712e922e05ea8b60
dpoulson <darren.poulson@gmail.com>
2021-12-08 11:22:49 +0000

    Reverted some changes to members admin page

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.app')
 
-@section('scripts')
-
-
 @section('content')
-<datatable-component>
-</datatable-component>
+
+<style>
+@media only screen and (max-width: 800px) {
+  td:nth-child(3) {
+    display:none;
+  }
+
+  th:nth-child(3) {
+    display:none;
+  }
+
+  td:nth-child(4) {
+    display:none;
+  }
+
+  th:nth-child(4) {
+    display:none;
+  }
+
+  td:nth-child(5) {
+    display:none;
+  }
+
+  th:nth-child(5) {
+    display:none;
+  }
+}
+</style>
+
 <div class="row">
   <div class="col-md-12">
     <div class="card">
@@ -13,10 +37,17 @@
         <h4 class="title text-center">Members</h4>
       </div>
       <div class="card-body table table-striped table-sm table-hover table-dark text-center">
-
+        {{$dataTable->table()}}
       </div>
     </div>
   </div>
 </div>
 
 @endsection
+
+@push('scripts')
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
+<script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
+<script src="/vendor/datatables/buttons.server-side.js"></script>
+    {{$dataTable->scripts()}}
+@endpush
