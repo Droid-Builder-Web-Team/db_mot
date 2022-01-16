34e8d7b04408a3d955f77c7f01d3253ed53386f5
dpoulson <darren.poulson@gmail.com>
2022-01-16 16:34:36 +0000

    Fix typo in mapscontroller

--- app/Http/Controllers/Admin/MapsController.php
+++ app/Http/Controllers/Admin/MapsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use app\User;
+use App\User;
 
 class MapsController extends Controller
 {
