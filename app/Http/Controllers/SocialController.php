5c8748822bb2eddfa0a1c89d2dc4110d0db312cf
dpoulson <darren.poulson@gmail.com>
2022-01-19 12:00:44 +0000

    Log which social provider was last used.

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Socialite Login Controller
+ * php version 7.4
+ *
+ * @category Controller
+ * @package  Controllers
+ * @author   Darren Poulson <darren.poulson@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://portal.droidbuilders.uk/
+ */
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,20 +19,42 @@ use Socialite;
 use App\User;
 use App\Notifications\NewUser;
 
-
+/**
+ * Achievements
+ *
+ * @category Class
+ * @package  Controllers
+ * @author   Darren Poulson <darren.poulson@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://portal.droidbuilders.uk/
+ */
 class SocialController extends Controller
 {
+    /**
+     * Redirect to socialite provider
+     *
+     * @param mixed $provider Name of the provider
+     *
+     * @return void
+     */
     public function redirect($provider)
     {
         return Socialite::driver($provider)->redirect();
     }
 
+    /**
+     * Callback function for provider
+     *
+     * @param mixed $provider Name of provider
+     *
+     * @return void
+     */
     public function callback($provider)
     {
 
         $getInfo = Socialite::driver($provider)->user();
 
-        if ($getInfo->email == null){
+        if ($getInfo->email == null) {
             toastr()->error('You must have an email registered with the account');
             return redirect()->to('/login');
         }
@@ -29,16 +62,29 @@ class SocialController extends Controller
         $user = $this->createUser($getInfo, $provider);
 
         if ($user->active == "on") {
-            auth()->login($user);
-        }
-        else
-        {
+            try {
+                $login = \Auth::login($user);
+            } catch(Exception $e) {
+                toastr()->error('Login error: ' . $e);
+            }
+        } else {
+            toastr()->error('Account inactive');
             return redirect()->to('/login');
         }
 
+        toastr()->success('Logging in');
         return redirect()->to('/user');
 
     }
+
+    /**
+     * Create User if they don't exist
+     *
+     * @param mixed $getInfo  Information provided by social provider
+     * @param mixed $provider Name of provider
+     *
+     * @return void
+     */
     function createUser($getInfo,$provider)
     {
 
@@ -63,8 +109,7 @@ class SocialController extends Controller
                     $q->where("name", "Super Admin");
                 }
             )->get();
-            foreach($admins as $admin)
-            {
+            foreach ($admins as $admin) {
                   $admin->notify(new NewUser($user));
             }
         }
@@ -72,7 +117,8 @@ class SocialController extends Controller
         $user->update(
             [
             'last_login' => now(),
-            'last_login_from' => \Request::ip()
+            'last_login_from' => \Request::ip(),
+            'last_login_type' => $provider
             ]
         );
         return $user;
