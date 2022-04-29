113848ba4d98580080438744272564f71ab81460
Darren Poulson <darren.poulson@gmail.com>
2022-04-29 13:47:28 +0100

    More fixes for facebook part run sharing

--- resources/views/parts-run/facebook.blade.php
+++ resources/views/parts-run/facebook.blade.php
@@ -1,7 +1,7 @@
     <!-- Facebook metadata for sharing events -->
     <meta property="og:url"           content="{{ URL::current() }}" />
     <meta property="og:type"          content="website" />
-    <meta property="og:title"         content="{{ $partsRunData->partsRunAd->title }}" />
-    <meta property="og:description"   content="{{ $partsRunData->partsRunAd->description }}" />
+    <meta property="og:title"         content="{{ $partsRunData[0]->partsRunAd->title }}" />
+    <meta property="og:description"   content="{{ $partsRunData[0]->partsRunAd->description }}" />
     <meta property="fb:app_id"        content="{{ config('fb.fb_app_id', 'Laravel') }}" />
     <!-- End of metadata -->
