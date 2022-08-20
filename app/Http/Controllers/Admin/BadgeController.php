f58beb88c5f4235290a945de4935f1b56a261170
Darren Poulson <darren.poulson@gmail.com>
2022-08-20 14:52:13 +0100

    Updates for downloading csv of new id badges.

--- app/Http/Controllers/Admin/BadgeController.php
+++ app/Http/Controllers/Admin/BadgeController.php
@@ -60,92 +60,116 @@ class BadgeController extends Controller
         $zip = new ZipArchive();
         $zip_file = "test.zip";
         $zip->open($zip_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+        $columns = array(
+            'Forename',
+            'Surname',
+            'Member ID',
+            'Email',
+            'PLI Paid',
+            'ReIssue',
+            'Droid ID'
+        );
+        $csv_filename = "id_list.csv";
+        $fp = fopen($csv_filename, 'w');
+        fputcsv($fp, $columns);
         foreach ($users as $userid) {
             $user = User::find($userid->user_id);
-            if ($user->validPLI() || ($userid->reissue && $userid->paid)) {
-                $dir = $user->forename . '_' . $user->surname;
-                $zip->addEmptyDir($dir);
 
-                // Get QR code image
-                $path = 'members/'.$user->id.'/qr_code.png';
-                $qr_extension = 'png';
-                if (!Storage::exists($path)) {
-                    $path = 'members/'.$user->id.'/qr_code.jpg';
-                    $qr_extension = 'jpg';
-                }
-                $qrcode = Storage::get($path);
+            $dir = $user->forename . '_' . $user->surname;
+            $zip->addEmptyDir($dir);
 
-                // Get Mug Shot image
-                $path = 'members/'.$user->id.'/mug_shot.png';
-                $mug_extension = 'png';
-                if (!Storage::exists($path)) {
-                    $path = 'members/'.$user->id.'/mug_shot.jpg';
-                    $mug_extension = 'jpg';
-                }
-                if (!Storage::exists($path)) {
-                    $path = getcwd().'/img/blank_mug_shot.jpg';
-                    $mugshot = file_get_contents($path);
-                    $mug_extension = "jpg";
-                } else {
-                    $mugshot = Storage::get($path);
-                }
+            // Get QR code image
+            $path = 'members/'.$user->id.'/qr_code.png';
+            $qr_extension = 'png';
+            if (!Storage::exists($path)) {
+                $path = 'members/'.$user->id.'/qr_code.jpg';
+                $qr_extension = 'jpg';
+            }
+            $qrcode = Storage::get($path);
 
-                // Add to zip file
-                $zip->addFromString(
-                    $dir . '/qr_code' . '.' .  $qr_extension,
-                    $qrcode
-                );
-                $zip->addFromString(
-                    $dir . '/mug_shot' . '.' .  $mug_extension,
-                    $mugshot . '.' .  $mug_extension
-                );
-
-                if ($userid->reissue) {
-                    $zip->addFromString(
-                        $dir . '/reissue.txt',
-                        'This is a reissue due to lost card or updating picture.'
-                    );
-                }
-
-                $droids = DB::table('id_list')
-                    ->where('user_id', $userid->user_id)
-                    ->get();
-
-                foreach ($droids as $droid) {
-                    // Get Mug Shot image
-                    $path = 'droids/'.$droid->droid_id.'/photo_front.png';
-                    $droid_extension = 'png';
-                    if (!Storage::exists($path)) {
-                        $path = 'droids/'.$droid->droid_id.'/photo_front.jpg';
-                        $droid_extension = 'jpg';
-                    }
-                    if (!Storage::exists($path)) {
-                        $path = getcwd().'/img/blank_photo_front.jpg';
-                        $droid_image = file_get_contents($path);
-                        $droid_extension = "jpg";
-                    } else {
-                        $droid_image = Storage::get($path);
-                    }
-
-                    $zip->addFromString(
-                        $dir . '/droid_' . $droid->droid_id . '.' .  $droid_extension,
-                        $droid_image . '.' .  $droid_extension
-                    );
-
-                    if ($clear == "true") {
-                        DB::table('id_list')
-                            ->where(
-                                [
-                                    [  'user_id', $userid->user_id ],
-                                    [ 'droid_id', $droid->droid_id ]
-                                ]
-                            )->delete();
-                    }
-                }
-
+            // Get Mug Shot image
+            $path = 'members/'.$user->id.'/mug_shot.png';
+            $mug_extension = 'png';
+            if (!Storage::exists($path)) {
+                $path = 'members/'.$user->id.'/mug_shot.jpg';
+                $mug_extension = 'jpg';
+            }
+            if (!Storage::exists($path)) {
+                $path = getcwd().'/img/blank_mug_shot.jpg';
+                $mugshot = file_get_contents($path);
+                $mug_extension = "jpg";
+            } else {
+                $mugshot = Storage::get($path);
             }
 
+            // Add to zip file
+            $zip->addFromString(
+                $dir . '/qr_code' . '.' .  $qr_extension,
+                $qrcode
+            );
+            $zip->addFromString(
+                $dir . '/mug_shot' . '.' .  $mug_extension,
+                $mugshot . '.' .  $mug_extension
+            );
+
+            if ($userid->reissue) {
+                $zip->addFromString(
+                    $dir . '/reissue.txt',
+                    'This is a reissue due to lost card or updating picture.'
+                );
+            }
+
+            $droids = DB::table('id_list')
+                ->where('user_id', $userid->user_id)
+                ->get();
+
+            foreach ($droids as $droid) {
+                // Get Mug Shot image
+                $path = 'droids/'.$droid->droid_id.'/photo_front.png';
+                $droid_extension = 'png';
+                if (!Storage::exists($path)) {
+                    $path = 'droids/'.$droid->droid_id.'/photo_front.jpg';
+                    $droid_extension = 'jpg';
+                }
+                if (!Storage::exists($path)) {
+                    $path = getcwd().'/img/blank_photo_front.jpg';
+                    $droid_image = file_get_contents($path);
+                    $droid_extension = "jpg";
+                } else {
+                    $droid_image = Storage::get($path);
+                }
+
+                $zip->addFromString(
+                    $dir . '/droid_' . $droid->droid_id . '.' .  $droid_extension,
+                    $droid_image . '.' .  $droid_extension
+                );
+
+                if ($clear == "true") {
+                    DB::table('id_list')
+                        ->where(
+                            [
+                                [  'user_id', $userid->user_id ],
+                                [ 'droid_id', $droid->droid_id ]
+                            ]
+                        )->delete();
+                }
+                fputcsv($fp, array(
+                    $user->forename,
+                    $user->surname,
+                    $user->id,
+                    $user->email,
+                    $user->validPLI(),
+                    $userid->reissue,
+                    $droid->droid_id
+                ));
+            }
+
+            
+
         }
+        fclose($fp);
+        $zip->addFile($csv_filename, 'id_list.csv');
+        $zip->close();
 
         $headers = array(
             'Content-Type' => 'application/zip',
