8f7980b880a9eb628152a21b58b3da85d2541e1e
Darren Poulson <darren.poulson@gmail.com>
2026-05-02 15:06:57 +0100

    Update auction creation to use all request data

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -86,7 +86,7 @@ class AuctionController extends Controller
         $request['finish_time'] = $request->finish_date . " " . $request->finish_time;
 
         try {
-            $auction = Auction::create($request->except(['id', 'user_id']));
+            $auction = Auction::create($request->all());
             flash()->addSuccess('Auction created successfully');
         } catch (\Illuminate\Database\QueryException $exception) {
             flash()->addError('Failed to create Auction');
@@ -173,7 +173,7 @@ class AuctionController extends Controller
 
         $request['finish_time'] = $request->finish_date . " " . $request->finish_time;
 
-        $newauction = $request->except(['id', 'user_id']);
+        $newauction = $request->all());
 
         #dd($newauction);
         $linkify = new \Misd\Linkify\Linkify();
